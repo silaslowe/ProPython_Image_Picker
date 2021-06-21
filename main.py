3a5f82dd3ae6165a2dc920dd0649f00193805ae3
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-06-21 12:46:03 -0500

    image loads on click from wikipedia

--- main.py
+++ main.py
@@ -2,12 +2,27 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
+import wikipedia
+import requests
+
 Builder.load_file('frontend.kv')
 
 
 class FirstScreen(Screen):
     def search_image(self):
-        self.manager.current_screen.ids.img.source = "images/road.jpeg"
+        # Get user input
+        query = self.manager.current_screen.ids.user_query.text
+        # Get wiki page
+        page = wikipedia.page(query)
+        image_link = page.images[0]
+        # Download image
+        req = requests.get(image_link)
+        image_path = 'images/image.jpg'
+
+        with open(image_path, 'wb') as file:
+            file.write(req.content)
+
+        self.manager.current_screen.ids.img.source = image_path
 
 
 class RootWidget(ScreenManager):
